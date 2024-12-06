44a4f331423070990b3356ff8792bc7826382b34
Ehte22 <syedehteshamali22@gmail.com>
2024-12-06 17:20:03 +0530

    websoket updated

--- client/src/pages/UserList.tsx
+++ client/src/pages/UserList.tsx
@@ -6,7 +6,9 @@ import { toast } from "../services/toast"
 import Pagination from "../components/Pagination"
 import io, { Socket } from "socket.io-client";
 
-const socket: typeof Socket = io(`${import.meta.env.VITE_BACKEND_URL}`);
+const socket: typeof Socket = io(`${import.meta.env.VITE_BACKEND_URL}`, {
+    transports: ["websocket"]
+});
 
 const UserList = () => {
     const gender = [
